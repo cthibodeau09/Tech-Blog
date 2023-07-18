79d044648f99031bee3a3f4c2cea943b8f234280
Little Bit <cthibodeau09@gmail.com>
2023-07-17 21:05:17 -0400

    created connection to db

--- config/connection.js
+++ config/connection.js
@@ -3,3 +3,13 @@ const Sequelize = require('sequelize');
 //allow for use of .env data
 require('dotenv').config();
 
+// create connection to db
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  });
+
+module.exports = sequelize;
